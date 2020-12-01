5d1607db870f2b5f23149d789ea0a80a5878f6d8
Ryan Matthew Gonzalez <ryanpiunj@gmail.com>
2020-12-01 04:46:40 -0500

    update #3

--- employeedb.sql
+++ employeedb.sql
@@ -6,14 +6,14 @@ USE employeedb;
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
-    salary DECIMAL(10,2),
-    department_id INT(10)
+    primary key (id)
 );
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(30),
-    department_id INT(10)
+    department_id INT,
+    primary key (id)
 );
 
 CREATE TABLE employee (
@@ -21,5 +21,5 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT(10),
-    manager_id INT(10) NULL
+    primary key (id)
 );
